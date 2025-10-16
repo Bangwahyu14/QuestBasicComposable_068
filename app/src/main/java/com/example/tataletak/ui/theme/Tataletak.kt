db0dfe5720236d869f69e9ec36e0dbc891342199
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-10-16 18:23:46 +0700

    Menambahkan teks 'My Music' dengan gaya huruf tebal dan posisi tengah

--- app/src/main/java/com/example/tataletak/ui/theme/Tataletak.kt
+++ app/src/main/java/com/example/tataletak/ui/theme/Tataletak.kt
@@ -153,3 +153,21 @@ fun TataLetakBoxColumnRow(modifier: Modifier) {
             .background(color = Color.Cyan),
         contentAlignment = Alignment.Center
     ) {
+        Image(
+            painter = gambar,
+            contentDescription = null,
+            contentScale = ContentScale.Fit
+        )
+
+        Text(
+            text = "My Music",
+            fontSize = 50.sp,
+            color = Color.Red,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Cursive,
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
+}
+
+
