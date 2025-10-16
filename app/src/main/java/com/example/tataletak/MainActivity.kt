e6b10f3ef94c191bdce20cf123c97e805d1f73a6
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-10-16 17:05:37 +0700

    ujicoba ke dua

--- app/src/main/java/com/example/tataletak/MainActivity.kt
+++ app/src/main/java/com/example/tataletak/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             TataLetakTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Andro",
+                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
