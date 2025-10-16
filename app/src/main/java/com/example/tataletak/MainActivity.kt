be6c54c035bdc25e8e62fc01f01b6b7ad17bf2ac
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-10-16 18:33:10 +0700

    Menambahkan MainActivity dengan Scaffold dan layout utama TataletakBoxColumnRow

--- app/src/main/java/com/example/tataletak/MainActivity.kt
+++ app/src/main/java/com/example/tataletak/MainActivity.kt
@@ -19,19 +19,19 @@ import com.example.tataletak.ui.theme.TataLetakTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            TataLetakTheme {
+            MyLayoutTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    // Panggil composable layout utama dengan padding dari Scaffold
+                    TataletakBoxColumnRow(
+                        modifier = Modifier.padding(paddingValues = innerPadding)
                     )
                 }
             }
         }
     }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
